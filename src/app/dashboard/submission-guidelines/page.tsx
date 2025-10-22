30d400f99b4fa90523b3311744917d82dc6cf3b4
Ken Thampiratwong <ken.thampiratwong@gmail.com>
2025-10-21 21:51:18 -0700

    fix: last commit error

--- src/app/dashboard/submission-guidelines/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import SubmissionsGuidelines from "@/components/Dashboard/SubmissionsGuidelines";
-
-export default function DashboardPage() {
-  return <SubmissionsGuidelines />;
-}
