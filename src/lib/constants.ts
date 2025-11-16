ef2800876ffb99dfa92c8b74149efe290b745b54
Aarush Narang <aarushnarang@gmail.com>
2025-11-15 16:40:51 -0800

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -15,7 +15,7 @@ export const SUBMISSION_MAX_FILE_SIZE = 10485760 as const; // 10 MB (10 * 1024 *
 // export const TABLE_SEATS_MIN_COUNT = 2;
 // export const TABLE_SEATS_MIN_START = 5;
 
-export const POLL_INTERVAL_MS = 10000 as const; // 10 seconds
+export const POLL_INTERVAL_MS = 5000 as const; // 10 seconds
 // export const TABLE_SEATS_MIN_COUNT = 1; // allow 1 for testing purposes
 // export const TABLE_SEATS_MIN_START = 1; // allow 1 for testing purposes
 
